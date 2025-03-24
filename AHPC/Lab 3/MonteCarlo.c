2dfb721996d829bda4ebd078dd0c1d8aece8bf5c
Jiahong Li <126418634+MediumSizedDeveloper@users.noreply.github.com>
2025-03-24 10:55:06 -0400

    Update MonteCarlo.c
    working updated code

--- AHPC/Lab 3/MonteCarlo.c
+++ AHPC/Lab 3/MonteCarlo.c
@@ -30,31 +30,32 @@ int main(int argc, char **argv){
 	}
 	//NOTICE: Bcast local to proc 0, will this cause error?
 	printf("Broadcasting from proc 0"); 
-	MPI_Bcast(x,N,MPI_DOUBLE,0,MPI_COMM_WORLD);
-	MPI_Bcast(y,N,MPI_DOUBLE,0,MPI_COMM_WORLD);	
+
     }
 
-    if(mypid!=0){
-        double z;
-	// WARNING: potential data loss due to integer division, 
-	// N/nprocs must be perfectly divisible
-	for(int i = mypid * (N/nprocs); i < (mypid+1) * (N/nprocs); i++){
-	    z = sqrt( x[i]*x[i] + y[i]*y[i] );
-	    if(z <= 1)
-	        localCount++;
-	}
-
-	//NOTICE: Reduce local to all procs other than 0, error?
-	MPI_Reduce(&localCount,&globalCount,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
-        printf("%d is the local count of proc %d",localCount,mypid);
+    MPI_Bcast(x,N,MPI_DOUBLE,0,MPI_COMM_WORLD);
+    MPI_Bcast(y,N,MPI_DOUBLE,0,MPI_COMM_WORLD);	
+ 
+    double z;
+    // WARNING: potential data loss due to integer division, 
+    // N/nprocs must be perfectly divisible
+    for(int i = mypid * (N/nprocs); i < (mypid+1) * (N/nprocs); i++){
+	z = sqrt( x[i]*x[i] + y[i]*y[i] );
+	if(z <= 1)
+	    localCount++;
     }
+
+    // NOTICE: Reduce local to all procs other than 0, error?
+    MPI_Reduce(&localCount,&globalCount,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
+    printf("\n%d is the local count of proc %d\n",localCount,mypid);
+    
 
     if(mypid==0){
 	// calculate pi with our points, multiplying by 4 because relationship
 	// between area of cirle/square
 	double pi = (double)globalCount/(double)N * 4.0;
 
-	printf("The value of pi is: %d", pi);
+	printf("The value of pi is: %f", pi);
     }
 
     MPI_Finalize();
